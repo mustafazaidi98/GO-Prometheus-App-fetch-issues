2051636b7796162944fd7be047abf3c30bfa4f70
mustafazaidi98 <39692019+mustafazaidi98@users.noreply.github.com>
2023-12-11 17:38:23 -0600

    updated script file

--- main.go
+++ main.go
@@ -1,90 +1,201 @@
 package main
 
 import (
-	"context"
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"strings"
 	"time"
 
-	"github.com/google/go-github/v39/github"
-	"github.com/milvus-io/milvus-sdk-go/v2/client"
-	"github.com/milvus-io/milvus-sdk-go/v2/entity"
-	"golang.org/x/oauth2"
+	_ "github.com/lib/pq"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 const (
-	githubToken    = "YOUR_GITHUB_TOKEN"
-	host           = "localhost"
-	port           = "19530"
-	collectionName = "github_issues"
-	dimension      = 300 // Adjust based on the dimensionality of your vectors
-	issuesDaysBack = 90
+	host     = "34.123.89.96"
+	port     = 5432
+	user     = "postgres"
+	password = "28899"
+	dbname   = "Assignment5"
 )
 
-func main() {
-	// Connect to Milvus
-	ctx := context.Background()
-	cli, err := client.NewGrpcClient(ctx, host, port)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer cli.Close(ctx)
-
-	// Authenticate with GitHub
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: githubToken},
+var (
+	githubAPICalls = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "github_api_calls_total",
+			Help: "Total number of GitHub API calls.",
+		},
+		[]string{"repository"},
 	)
-	tc := oauth2.NewClient(ctx, ts)
-	client := github.NewClient(tc)
 
-	// Specify the repositories
-	repos := []string{"facebook/react", "angular/angular", "tensorflow/tensorflow"}
+	stackOverflowAPICalls = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "stackoverflow_api_calls_total",
+			Help: "Total number of StackOverflow API calls.",
+		},
+		[]string{"tag"},
+	)
 
-	// Fetch and store GitHub issues for each repository
-	for _, repo := range repos {
-		// Fetch GitHub issues
-		issues, err := fetchGitHubIssues(client, repo, issuesDaysBack)
-		if err != nil {
-			log.Printf("Error fetching issues for %s: %v\n", repo, err)
-			continue
-		}
+	dataCollected = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "data_collected_total",
+			Help: "Total amount of data collected.",
+		},
+		[]string{"source"},
+	)
 
-		// Create a collection if it does not exist
-		collectionParam := entity.NewCollectionParam(collectionName, dimension)
-		collection, err := cli.HasCollection(ctx, collectionName)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if !collection {
-			err := cli.CreateCollection(ctx, collectionName, collectionParam)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
+	totalDataSize float64
+)
 
-		// Insert vectors into Milvus
-		for _, issue := range issues {
-			vector := getVectorFromIssue(issue)
-			_, err := cli.Insert(ctx, collectionName, [][]float32{vector})
-			if err != nil {
-				log.Printf("Error inserting vector for %s: %v\n", repo, err)
-				continue
-			}
-		}
-
-		fmt.Printf("Vectors for %s inserted successfully!\n", repo)
-	}
+type gitIssue struct {
+	Title string `json:"title"`
+	Link  string `json:"html_url"`
+	Size  float64
 }
 
-func fetchGitHubIssues(client *github.Client, repo string, daysBack int) ([]*github.Issue, error) {
-	// Calculate the date 90 days ago
-	since := time.Now().AddDate(0, 0, -daysBack)
+type StackOverflowItem struct {
+	Items []struct {
+		QuestionID int    `json:"question_id"`
+		Title      string `json:"title"`
+		IsAnswered bool   `json:"is_answered"`
+		Answers    []struct {
+			AnswerID   int  `json:"answer_id"`
+			IsAccepted bool `json:"is_accepted"`
+		} `json:"answers"`
+	} `json:"items"`
+}
 
-	// Fetch GitHub issues
-	issues, _, err := client.Issues.ListByRepo(context.Background(), ownerFromRepo(repo), repoNameFromRepo(repo), &github.IssueListByRepoOptions{
-		Since: &since,
-	})
+func main() {
+	prometheus.MustRegister(githubAPICalls)
+	prometheus.MustRegister(stackOverflowAPICalls)
+	prometheus.MustRegister(dataCollected)
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		http.ListenAndServe(":8081", nil)
+	}()
+	go func() {
+		for {
+			fetchAndProcessData()
+			time.Sleep(1000 * time.Microsecond)
+		}
+	}()
+	select {}
+}
+
+func fetchAndProcessData() {
+	token := "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"
+
+	urls := []string{
+		"https://api.github.com/repos/golang/go/issues",
+		"https://api.github.com/repos/prometheus/prometheus/issues",
+		"https://api.github.com/repos/seleniumhq/selenium/issues",
+		"https://api.github.com/repos/openai/openai-python/issues",
+		"https://api.github.com/repos/docker/docs/issues",
+		"https://api.github.com/repos/milvus-io/milvus/issues",
+	}
+
+	stackoverflowLinks := []string{
+		"https://api.stackexchange.com/2.3/search/advanced?site=stackoverflow&tagged=go",
+		"https://api.stackexchange.com/2.3/search/advanced?site=stackoverflow&tagged=prometheus",
+		"https://api.stackexchange.com/2.3/search/advanced?site=stackoverflow&tagged=selenium",
+		"https://api.stackexchange.com/2.3/search/advanced?site=stackoverflow&tagged=openai",
+		"https://api.stackexchange.com/2.3/search/advanced?site=stackoverflow&tagged=docker",
+		"https://api.stackexchange.com/2.3/search/advanced?site=stackoverflow&tagged=milvus",
+	}
+
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		fmt.Println("Error connecting to the database:", err)
+		return
+	}
+	defer db.Close()
+
+	for _, url := range urls {
+		githubAPICalls.WithLabelValues(repositoryName(url)).Inc()
+
+		issues, err := fetchGitHubIssues(url, token)
+		if err != nil {
+			fmt.Println("Error fetching GitHub issues:", err)
+			continue
+		}
+		const githubIssueSize = 0.1
+		for i := range issues {
+			issues[i].Size = githubIssueSize
+		}
+		for _, issue := range issues {
+			totalDataSize += issue.Size
+		}
+
+		err = insertIntoDatabase(db, issues)
+		if err != nil {
+			fmt.Println("Error inserting into the database:", err)
+			continue
+		}
+		dataCollected.WithLabelValues("github").Add(totalDataSize)
+	}
+
+	for _, link := range stackoverflowLinks {
+		stackOverflowAPICalls.WithLabelValues(tagName(link)).Inc()
+		fmt.Println(link)
+		item, err := fetchDataFromStackOverflow(link)
+		if err != nil {
+			log.Println("Error fetching data from Stack Overflow:", err)
+			continue
+		}
+		for _, i := range item.Items {
+			fmt.Sprintf("Question ID: %d\nTitle: %s\nIs Answered: %t\n", i.QuestionID, i.Title, i.IsAnswered)
+			err := insertDataStackOverFLow(db, i.QuestionID, i.Title, i.IsAnswered)
+			if err != nil {
+				fmt.Println("Error inserting into the database:", err)
+			}
+		}
+		dataCollected.WithLabelValues("stackoverflow").Add(totalDataSize)
+	}
+}
+func repositoryName(url string) string {
+	parts := strings.Split(url, "/")
+	if len(parts) >= 4 {
+		return parts[4] + "/" + parts[5]
+	}
+	return "unknown"
+}
+func tagName(link string) string {
+	parts := strings.Split(link, "=")
+	if len(parts) >= 2 {
+		return parts[2]
+	}
+	return "unknown"
+}
+
+func fetchGitHubIssues(url, token string) ([]gitIssue, error) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", "token "+token)
+	req.Header.Set("Accept", "application/vnd.github.v3+json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var issues []gitIssue
+	err = json.Unmarshal(body, &issues)
 	if err != nil {
 		return nil, err
 	}
@@ -92,17 +203,47 @@ func fetchGitHubIssues(client *github.Client, repo string, daysBack int) ([]*git
 	return issues, nil
 }
 
-func ownerFromRepo(repo string) string {
-	return repo[:strings.Index(repo, "/")]
+func fetchDataFromStackOverflow(link string) (*StackOverflowItem, error) {
+	req, err := http.NewRequest("GET", link+"", nil)
+	if err != nil {
+		return nil, err
+	}
+	q := req.URL.Query()
+	q.Add("client_id", "Krf9*ha)ZXEeQmLlVEgubQ((")
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var item StackOverflowItem
+	err = json.Unmarshal(body, &item)
+	fmt.Println(item)
+	if err != nil {
+		return nil, err
+	}
+	return &item, nil
 }
 
-func repoNameFromRepo(repo string) string {
-	return repo[strings.Index(repo, "/")+1:]
+func insertIntoDatabase(db *sql.DB, issues []gitIssue) error {
+	for _, issue := range issues {
+		_, err := db.Exec(`INSERT INTO public."GitIssues"("Title", "url") VALUES ($1, $2)`, issue.Title, issue.Link)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
-func getVectorFromIssue(issue *github.Issue) []float32 {
-	// Implement a function to convert the issue text into vectors
-	// This can involve using pre-trained word embeddings or other techniques
-	// Here, we're using a placeholder implementation for illustration
-	return make([]float32, dimension)
+func insertDataStackOverFLow(db *sql.DB, questionID int, title string, isAnswered bool) error {
+	sqlStatement := `
+		INSERT INTO public."StackOverFlow"("QuestionID", "Title", "isAnswered")
+		VALUES ($1, $2, $3)`
+	_, err := db.Exec(sqlStatement, questionID, title, isAnswered)
+	return err
 }
